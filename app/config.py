1cc225a19541a6592b48f6096d80320f24ea36ca
agarin3@gmail.com <agarin3@gmail.com>
2024-07-22 18:36:23 +0100

    created my database using orm

--- app/config.py
+++
@@ -1,4 +0,0 @@
-import os
-
-class Config:
-    SECRET_KEY = os.environ.get('ghostmode')  or 'you-will-never-guess'
--- config.py
+++ config.py
@@ -1,4 +1,7 @@
 import os
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config:
-    SECRET_KEY = os.environ.get('ghostmode')  or 'you-will-never-guess'
+    SECRET_KEY = os.environ.get('ghostmode')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+        'sqlite:///' + os.path.join(basedir, 'app.db')
