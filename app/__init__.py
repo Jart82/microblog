927c1577cb054e34b6fce29a796139acc95f6afe
agarin3@gmail.com <agarin3@gmail.com>
2024-07-31 18:03:12 +0100

    chapter 13: adding language translation to page

--- app/__init__.py
+++ app/__init__.py
@@ -1,15 +1,20 @@
 import logging
-from logging.handlers import SMTPHandler
-from logging.handlers import RotatingFileHandler
+from logging.handlers import SMTPHandler, RotatingFileHandler
 import os
-
-from flask import Flask
-from config import Config 
+from flask import Flask, request
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
 from flask_mail import Mail
 from flask_moment import Moment
+from flask_babel import Babel, lazy_gettext as _l
+from config import Config
+
+
+def get_locale():
+    # return 'es'
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -17,14 +22,15 @@ db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 login = LoginManager(app)
 login.login_view = 'login'
+login.login_message = _l('Please log in to access this page.')
 mail = Mail(app)
 moment = Moment(app)
-
+babel = Babel(app, locale_selector=get_locale)
 
 if not app.debug:
     if app.config['MAIL_SERVER']:
         auth = None
-        if app.config['MAIL_USERNAME'] or app.config['MAIL_PORT']:
+        if app.config['MAIL_USERNAME'] or app.config['MAIL_PASSWORD']:
             auth = (app.config['MAIL_USERNAME'], app.config['MAIL_PASSWORD'])
         secure = None
         if app.config['MAIL_USE_TLS']:
@@ -48,8 +54,6 @@ if not app.debug:
 
     app.logger.setLevel(logging.INFO)
     app.logger.info('Microblog startup')
-        
-
 
 
 from app import routes, models, errors
